51b7ce254dd1079603a5ff1b5f0d1d8368b8dbf7
JIMICODE <1159081156@qq.com>
2017-01-24 12:04:27 +0800

    Class GCCRandom declared

--- GCC_TOOL/GCCTOOL.h
+++ GCC_TOOL/GCCTOOL.h
@@ -1,1 +1,17 @@
 #pragma once
+#ifndef GCC_TOOL
+#define GCC_TOOL
+
+class GCCRandom
+{
+public:
+	GCCRandom();
+	~GCCRandom();
+	unsigned int Random(unsigned int range);
+	double Random();
+	void SetRandomSeed(unsigned int seed);
+	unsigned int GetRandomSeed();
+private:
+
+};
+#endif // !GCC_TOOL
