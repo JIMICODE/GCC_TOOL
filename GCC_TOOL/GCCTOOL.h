8113bf48a9445cd80d22d39b7b7909bdd576a8cf
JIMICODE <1159081156@qq.com>
2017-01-25 19:45:45 +0800

    Try

--- GCC_TOOL/GCCTOOL.h
+++ GCC_TOOL/GCCTOOL.h
@@ -1,6 +1,52 @@
 #pragma once
 #ifndef GCC_TOOL
 #define GCC_TOOL
+//include head file and library
+#include<map>
+#include<string>
+#include<memory>
+#include<tinyxml.h>
+//Front class delcaler
+class ActorFactory;
+class ActorComponent;
+
+//type define 
+typedef unsigned long ActorId;
+typedef unsigned long CompentId;
+typedef ActorComponent *(*ActorComponentCreator)(void);
+typedef std::map<std::string, ActorComponentCreator> ActorComponentCreatorMap;
+typedef std::shared_ptr<Actor> StrongActorPtr;
+typedef std::shared_ptr<ActorComponent> StrongActorComponentPtr;
+
+//class define
+
+//Compent base class
+class Actor
+{
+	friend class ActorFactory;
+private:
+	ActorId m_id;
+public:
+	const ActorId GetId(void) const { return m_id; }
+};
+
+class ActorComponent
+{
+
+};
+
+class ActorFactory
+{
+private:
+	ActorId m_lastActorId;
+	ActorId GetNextActorId(void) { ++m_lastActorId; return m_lastActorId; }
+protected:
+	ActorComponentCreatorMap m_actorComponentCreators;
+	virtual StrongActorComponentPtr CreateComponent(TiXmlAttribute);
+public:
+	ActorFactory(void);
+	StrongActorPtr CreateActor(const char* actorResource);
+};
 
 class GCCRandom
 {
